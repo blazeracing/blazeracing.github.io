97886a0cd5bdfffcf76ef30b35a8cd2f2ba12ffc
theelectricfire7 <androidisawesome.apk@gmail.com>
2017-10-23 01:04:59 +1100

    rawr

--- JS/CommonJS.js
+++ JS/CommonJS.js
@@ -11,6 +11,9 @@ function isRunning(){ //run in console to check whether this script is running.
   console.log("Is running");
 }
 
+$("body").on("click", "#social-ig", function(){
+  window.location = "https://instagram.com/ghsblazeracing";
+});
 
 $(window).scroll(function() {
   if($(window).scrollTop() > ($(".first-parallax").offset().top + $(".first-parallax").outerHeight()) ){
