0364d8294a94524aff3ef09dd740fd5069ed25b4
theelectricfire7 <androidisawesome.apk@gmail.com>
2017-10-24 00:49:26 +1100

    litcoin

--- JS/Spread.js
+++ JS/Spread.js
@@ -1,27 +1,38 @@
-var gridMap = {
-  0: [],
-  1: [],
-  2: [],
-  3: [],
-  4: [],
-  5: [],
-  6: [],
-};
+var gridMap = [];
 
 var activeCell;
-var fireSpeed = 0.1;
-var cellSpeed = 0.1;
+var fireSpeed = 1.07;
+var cellSpeed = 1;
 
 var keepTime = false;
 var timer = 0;
+var finalTime = 0;
+var cellCount = 0;
 
 function g2d(r,c){
-  return (r*7) + c;
+  return (r*6) + c;
 }
+
+function r3d(n){
+  return (n - (n % 6))/6;
+}
+
+function c3d(n){
+  return n%6;
+}
+
+function opac(c,o){
+  return 'rgba(' + parseInt(c.slice(-6,-4),16)
+    + ',' + parseInt(c.slice(-4,-2),16)
+    + ',' + parseInt(c.slice(-2),16)
+    +','+o+')';
+}
+
 
 $("#btn-play").click(function(){
   $("#game-entry-form").hide();
   $("#head-spacer").hide();
+  $("#game-space").show();
   $("#game-instruction").html("Click on a cell in the grid to start your fire.");
 });
 
@@ -29,25 +40,65 @@ $("#game-grid").on("click",".grid-table-cell", function(){
   if(!keepTime){
     keepTime = true;
   }
-  if(activeCell == undefined){
+  if((activeCell == undefined && gridMap[parseInt($(this).attr("id").substr(2),10)] == false) || cellCount == 0 ){
+    $("#game-instruction").html("Wait for the cell to burn!");
+    cellCount++;
     activeCell = parseInt($(this).attr("id").substr(2),10);
-    console.log(activeCell);
-    setTimeout(function(){ activeCell = undefined; }, 6000);
+    gridMap[activeCell] = 0.02;
+    console.log(cellCount);
+
+
   }
 });
 
-setInterval(function(){
+setInterval(function(){ //Game loop
   if(keepTime){
     timer+= 0.1;
     $("#game-timer").html("Time: " + Math.floor(timer));
+
+    if(activeCell !== undefined){
+      gridMap[activeCell] = gridMap[activeCell] * fireSpeed * cellSpeed;
+      if(gridMap[activeCell]>=1){
+        gridMap[activeCell] = 1;
+        cellSpeed = 1;
+        adjacentUnlock(activeCell);
+        setTimeout(function(){
+          activeCell = undefined;
+          if(cellCount==1){
+            $("#game-instruction").html("Click a cell adjacent to a burning cell to grow your fire.");
+          }
+          else if (cellCount == 36){
+            keepTime = false;
+            finalTime = parseFloat(timer.toFixed(3));
+            $("#game-instruction").html("Congratulations! You finished with a time of " +finalTime +  " seconds! Refresh the page to try again.");
+          }
+          else{
+            $("#game-instruction").html("Click a cell next to a lit cell to keep spreading the fire!");
+          }
+        }, 50);
+      }
+      $("#td"+activeCell).css("background-color", opac("#e03e53", gridMap[activeCell]));
+    }
   }
 }, 100);
 
 $(document).ready(function(){
-  for(var i = 0; i<7; i++){
+  $("#game-space").hide();
+  for(var i = 0; i<6; i++){
     $("#game-grid").append("<tr id = 'tr"+i+"'></tr>");
-    for(var j = 0; j<7;j++){
-      $("#tr"+i).append("<td class = 'grid-table-cell' id = 'td"+g2d(i,j)+"'>t</td>");
+    for(var j = 0; j<6;j++){
+      $("#tr"+i).append("<td class = 'grid-table-cell' id = 'td"+g2d(i,j)+"'></td>");
     }
   }
 });
+
+function adjacentUnlock(n){
+  gridMap[g2d(r3d(n)-1,c3d(n-1))] = false;
+  gridMap[g2d(r3d(n)-1,c3d(n))] = false;
+  gridMap[g2d(r3d(n)-1,c3d(n+1))] = false;
+  gridMap[g2d(r3d(n),c3d(n-1))] = false;
+  gridMap[g2d(r3d(n),c3d(n+1))] = false;
+  gridMap[g2d(r3d(n)+1,c3d(n-1))] = false;
+  gridMap[g2d(r3d(n)+1,c3d(n))] = false;
+  gridMap[g2d(r3d(n)+1,c3d(n+1))] = false;
+}
