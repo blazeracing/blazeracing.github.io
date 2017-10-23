4b9426595747feea88a2c0b540d09bae8c13d948
theelectricfire7 <androidisawesome.apk@gmail.com>
2017-10-23 21:48:52 +1100

    HotAndSpicy

--- JS/Spread.js
+++ JS/Spread.js
@@ -12,21 +12,36 @@ var activeCell;
 var fireSpeed = 0.1;
 var cellSpeed = 0.1;
 
+var keepTime = false;
+var timer = 0;
+
 function g2d(r,c){
   return (r*7) + c;
 }
 
 $("#btn-play").click(function(){
   $("#game-entry-form").hide();
+  $("#head-spacer").hide();
+  $("#game-instruction").html("Click on a cell in the grid to start your fire.");
 });
 
 $("#game-grid").on("click",".grid-table-cell", function(){
+  if(!keepTime){
+    keepTime = true;
+  }
   if(activeCell == undefined){
     activeCell = parseInt($(this).attr("id").substr(2),10);
     console.log(activeCell);
     setTimeout(function(){ activeCell = undefined; }, 6000);
   }
 });
+
+setInterval(function(){
+  if(keepTime){
+    timer+= 0.1;
+    $("#game-timer").html("Time: " + Math.floor(timer));
+  }
+}, 100);
 
 $(document).ready(function(){
   for(var i = 0; i<7; i++){
